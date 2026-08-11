7b0d78dc4194139784f49918208cbb11a41650de
nicoly-athayde <nicolyathayde4@gmail.com>
2026-08-11 10:45:33 -0300

    Continuação

--- todo-angular/src/app/services/todo-store.ts
+++ todo-angular/src/app/services/todo-store.ts
@@ -1,6 +1,57 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import { Todo } from '../models/todo';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TodoStore {}
+export class TodoStore {
+  private readonly storageKey = 'todos-angular';
+  private readonly todosState = signal<Todo[]>(this.loadTodos());
+
+  readonly todos = this.todosState.asReadonly();
+  readonly total = computed(() => this.todosState().length);
+  readonly completed = computed(
+    () => this.todosState().filter((todos) => todos.completed).length);
+
+    readonly pending = computed(() => this.total() - this.completed());
+
+    add(title: string): void {
+      const normalizedTitle = title.trim();
+
+      if (!normalizedTitle) {
+        return;
+      }
+
+      const newTodo: Todo = {
+        id: Date.now(),
+        title: normalizedTitle,
+        completed: false
+      }
+
+      this.updateTodos([...this.todosState(), newTodo]);
+    }
+
+    private updateTodos(todos: Todo[]) : void {
+      this.todosState.set(todos);
+      localStorage.setItem(this.storageKey, JSON.stringify(todos));
+    }
+
+
+  private loadTodos(): Todo[] {
+    const savedTodos = localStorage.getItem(this.storageKey);
+
+    if (!savedTodos) {
+      return [];
+    }
+
+    try {
+      return JSON.parse(savedTodos) as Todo[];
+    } catch {
+      localStorage.removeItem(this.storageKey);
+      return [];
+    }
+
+  }
+
+
+}
